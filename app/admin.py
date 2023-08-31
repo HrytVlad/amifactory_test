20ed56a5c7b69bf6e07cdbba804e851231f3c575
HrytVlad <sunrises2929@gmail.com>
2023-08-31 16:12:40 +0300

    Register models for admin panel

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.models import Movie, Genre, Person
+
+admin.site.register(Movie)
+admin.site.register(Genre)
+admin.site.register(Person)
