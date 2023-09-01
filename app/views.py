57c0fa7f77b65c7a4f7f4ba8c6736fb8f6b0d257
HrytVlad <sunrises2929@gmail.com>
2023-09-01 13:22:55 +0300

    fixed code duplication and made it more concise

--- app/views.py
+++ app/views.py
@@ -2,33 +2,15 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.core.paginator import EmptyPage, Paginator
 from django.http import JsonResponse
 from django.views import generic, View
-
+from app.responses import get_movie_response, get_genre_response
 from app.models import Genre, Movie
-
-
-def convert_data(movie: Movie) -> dict:
-    return {
-        "id": movie.id,
-        "title": movie.title,
-        "description": movie.description,
-        "release_year": movie.release_year,
-        "mpa_rating": movie.mpa_ratings,
-        "imdb_rating": float(movie.imdb_rating),
-        "duration": movie.duration,
-        "poster": movie.poster.url,
-        "bg_picture": movie.bg_picture.url,
-        "genres": list(movie.genres.values("id", "title")),
-        "directors": list(movie.directors.values("id", "first_name", "last_name")),
-        "writers": list(movie.writers.values("id", "first_name", "last_name")),
-        "stars": list(movie.stars.values("id", "first_name", "last_name")),
-    }
+from app.validation import movie_list_validator
 
 
 class GenresListView(generic.ListView):
-    model = Genre
-
     def get(self, request, pk=None, *args, **kwargs):
-        data = list(self.model.objects.values("id", "title"))
+        genres = Genre.objects.all()
+        data = [get_genre_response(genre) for genre in genres]
         return JsonResponse(data, safe=False)
 
 
@@ -37,28 +19,28 @@ class MovieDetailView(View):
     def get(self, request, pk, *args, **kwargs):
         try:
             movie = Movie.objects.get(pk=pk)
-            movie_dict = convert_data(movie)
-            return JsonResponse(movie_dict, safe=False)
+            movie_dict = get_movie_response(movie)
+            return JsonResponse(movie_dict, safe=False, status=200)
         except ObjectDoesNotExist:
             return JsonResponse({"error": ["movie__not_found"]}, status=404)
 
 
 class MoviesListView(View):
-
     def get(self, request, *args, **kwargs):
         genre_id = request.GET.get("genre")
         src = request.GET.get("src")
         page = request.GET.get("page")
 
+        validation_result = movie_list_validator(genre_id, src, page)
+        if validation_result:
+            return validation_result
+
         queryset = Movie.objects.all()
         if genre_id:
-            queryset = queryset.filter(genres__id=genre_id)
-
-            if not queryset.exists():
-                return JsonResponse({"error": ["genre__invalid"]}, status=404)
+            queryset = queryset.filter(genres__id=genre_id).order_by('id')
 
         if src and 2 <= len(src) <= 20:
-            queryset = queryset.filter(title__istartswith=src)
+            queryset = queryset.filter(title__istartswith=src).order_by('id')
 
         paginator = Paginator(queryset, 5)
         try:
@@ -66,11 +48,12 @@ class MoviesListView(View):
         except EmptyPage:
             return JsonResponse({"error": ["page__out_of_bounds"]}, status=404)
 
-        movie_dicts = [convert_data(movie) for movie in movies]
+        movie_dicts = [get_movie_response(movie) for movie in movies]
 
         response_data = {
             "pages": paginator.num_pages,
             "total": len(movie_dicts),
             "results": movie_dicts,
         }
+
         return JsonResponse(response_data)
